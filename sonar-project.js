b20c3df9392f77920bbff4fd2b6f8cf86f36ec3f
PRATHIMA <95534720+prathima-design@users.noreply.github.com>
2022-03-01 21:52:34 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.108.66.166:9000/',
+  serverUrl: 'http://65.2.38.148:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '92234d5e28d98d74e686e5c3808a3308b96b74d8',
+	    'sonar.login': '3979490ce80502cef12f3e73bb2a977627823491',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
